d39a85c2a344f5dcad076b964ef0ecbd817b8d12
Matei Mircea <mrc_matei@yahoo.com>
2020-11-04 22:44:26 +0200

    another challenge added

--- script.js
+++ script.js
@@ -30,3 +30,42 @@ var numereLipsa = (arr) => {
     return missing;
 }
 console.log(numereLipsa([0, 2, 5, 9, 11]));
+
+
+
+// CHALLENGE 3
+
+//Count occurences in an array
+var frecventa = (arr) => {
+    //first solution
+    let count = {};
+    for (let i = 0; i < arr.length; i++) {
+        let num = arr[i];
+        console.log(i, num, count);
+        count[arr[i]] = count[arr[i]] ? count[arr[i]] + 1 : 1;
+    }
+
+    //second solution
+    // return arr.reduce((acc, elem) => {
+    //     console.log(val, elem, acc);
+    //     let a;
+    //     if (val === elem) {
+    //         a = acc + 1;
+    //     } else {
+    //         a = acc;
+    //     }
+    //     return a;
+    // }, 0);
+    return count;
+}
+console.log(frecventa([7, 7, 2, 1, 3, 4]));
+
+
+
+// CHALLENGE 4
+
+//Remove duplicates from an array
+let duplicates = (arr) => {
+
+}
+console.log(duplicates([1, 2, 5, 1, 1, 8]));
