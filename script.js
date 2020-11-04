72ef5f6c0fdf291af4d730114ddac08f72f6d4d7
Matei Mircea <mrc_matei@yahoo.com>
2020-11-04 17:36:00 +0200

    first challenges added

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 "use strict";
-console.log("Hello");
+//console.log("Hello");
 
+// CHALLENGE 1
+
+// PALIDROME
+// Given a string determine whether it is a palidrome
+var palidrome = (string) => {
+    var sanitazedString = string.toLowerCase().trim();
+    return sanitazedString === sanitazedString.split('').reverse().join('');
+}
+console.log(palidrome('   iyeLLeYI'));
+
+
+
+// CHALLENGE 2
+
+//MISSING NUMBERS
+//Given an array find the missing numbers and display them
+var numereLipsa = (arr) => {
+    let missing = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i + 1] - arr[i] > 1) {
+            for (let j = 1; j < arr[i + 1] - arr[i]; j++) {
+                //console.log(arr[i], i, arr[j], j);
+                missing.push(arr[i] + j);
+            }
+        }
+    }
+    return missing;
+}
+console.log(numereLipsa([0, 2, 5, 9, 11]));
