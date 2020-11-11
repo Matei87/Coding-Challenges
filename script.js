983b4aa6a63c2e4d8e1232a9d2ed3764d504d1e6
Matei Mircea <mrc_matei@yahoo.com>
2020-11-11 23:56:43 +0200

    another challenge

--- script.js
+++ script.js
@@ -338,3 +338,31 @@ const twoSum = (arr, target) => {
     console.log(arr, target);
 }
 console.log('twoSum', twoSum([4, -1, 0, 1, 5, 6, 8, 10], 9));
+
+
+
+
+// CHALLENGE 16
+// Older Than Me
+// Create a method in the Person class which returns how another person's age compares. Given the instances p1, p2 and p3, which will be initialised with the attributes name and age, return a sentence in the following format:
+
+// {other person name} is {older than / younger than / the same age as} me.
+// Examples
+// p1 = Person("Samuel", 24)
+// p2 = Person("Joel", 36)
+// p3 = Person("Lily", 24)
+
+// p1.compareAge(p2) ➞ "Joel is older than me."
+// p2.compareAge(p1) ➞ "Samuel is younger than me."
+// p1.compareAge(p3) ➞ "Lily is the same age as me."
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    compareAge(other) {
+        // Write code here!
+
+    }
+}
