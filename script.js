814aca49fca19390b3021f6bdc946e70f31e19b0
Matei Mircea <mrc_matei@yahoo.com>
2021-01-26 23:45:37 +0200

    another challenge added

--- script.js
+++ script.js
@@ -498,3 +498,19 @@ const numberToPower = (number, power) => {
     return number ** power;
 }
 console.log('numberToPower', numberToPower(2, 3));
+
+
+
+// CHALLENGE 23
+// Define a function that takes one integer argument and returns logical value true or false
+// depending on if the integer is a prime.
+// Per Wikipedia, a prime number (or a prime) is a natural number greater than 1 that has no
+// positive divisors other than 1 and itself.
+const isPrime = (num) => {
+    for (let i = 2; i < num; i++)
+        if (num % i === 0) {
+            return false;
+        }
+    return num > 1;
+}
+console.log('isPrime', isPrime(4));
