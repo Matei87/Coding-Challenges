c3826a9787bf549b47c8ab68a9b63f0d1a2d1c73
Matei Mircea <mircea.matei@hogarthww.com>
2021-06-16 20:17:17 +0300

    another challenge added

--- script.js
+++ script.js
@@ -1144,3 +1144,30 @@ function toTime(seconds) {
     return `${Math.floor(hours)} hour(s) and ${minutes} minute(s)`
 }
 console.log('toTime ', toTime(3500));
+
+
+
+
+// CHALLENGE 52
+// Highest and Lowest
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+// Example:
+// highAndLow("1 2 3 4 5");  // return "5 1"
+// highAndLow("1 2 -3 4 5"); // return "5 -3"
+// highAndLow("1 9 3 4 -5"); // return "9 -5"
+// Notes:
+//     All numbers are valid Int32, no need to validate them.
+//     There will always be at least one number in the input string.
+//     Output string must be two numbers separated by a single space, and highest number is first.
+function highAndLow(numbers){
+    //console.log('numbers ', numbers, numbers.split(' '));
+    let a = numbers.split(' ');
+    //console.log('a ', a);
+    let b = a.map(num => Number(num))
+    //console.log('b ', b);
+    let c = b.sort( (d, e) => d - e)
+    //console.log('c ', c);
+    //console.log(c[0], c[c.length - 1]);
+   return c[c.length - 1] + ' ' + c[0];
+}
+console.log('highAndLow ', highAndLow("4 5 29 54 4 0 -214 542 -64 1 -3 6 -6"));
